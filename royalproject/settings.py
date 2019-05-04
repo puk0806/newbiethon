3ab3c1718b42fd104087506d5ca59da051bacf39
chanho <puk0806@naver.com>
2019-05-05 05:28:02 +0900

    2nd commit addservice

--- royalproject/settings.py
+++ royalproject/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'royalproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'royalproject/templates'), ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- royalproject/urls.py
+++ royalproject/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+import board.views
 from django.conf import settings
 from django.conf.urls.static import static
 
 
 urlpatterns = [
+    path("",include('board.urls')),
     path('admin/', admin.site.urls),
     path('board/', include('board.urls')),
 ]
