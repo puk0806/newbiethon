6f097a9fe436bc64349c3fe9576f929efdccb883
chanho <puk0806@naver.com>
2019-05-14 21:16:53 +0900

    3rd commit add_loginservice

--- royalproject/urls.py
+++ royalproject/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 import board.views
+import accounts.views
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -9,4 +10,5 @@ urlpatterns = [
     path("",include('board.urls')),
     path('admin/', admin.site.urls),
     path('board/', include('board.urls')),
+    path('accounts/', include('accounts.urls')),
 ]
